56c4f3cc1f264a578f26bb92184dc99def0551aa
Muhammed Kerem <keremdmrbnt03@gmail.com>
2025-11-26 23:26:54 +0300

    bistler yazıldı

--- StockExchange.Designer.cs
+++ StockExchange.Designer.cs
@@ -41,14 +41,7 @@
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-
-
-            this.label6 = new System.Windows.Forms.Label();
-
-            this.label5 = new System.Windows.Forms.Label();
-
             ((System.ComponentModel.ISupportInitialize)(this.hisse_gosterme_gridview)).BeginInit();
             this.SuspendLayout();
             // 
@@ -185,64 +178,21 @@
             this.label2.TabIndex = 8;
             this.label2.Text = "label2";
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(130, 74);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 16);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "label3";
-            // 
             // label4
             // 
             this.label4.AutoSize = true;
-
-            this.label4.Location = new System.Drawing.Point(541, 74);
-
             this.label4.Location = new System.Drawing.Point(508, 74);
-
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(44, 16);
             this.label4.TabIndex = 10;
             this.label4.Text = "label4";
             // 
-
-            // label5
-            // 
-            
-
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(64, 74);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(44, 16);
-            this.label6.TabIndex = 12;
-            this.label6.Text = "label6";
-            // 
-
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(217, 74);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(44, 16);
-            this.label5.TabIndex = 13;
-            this.label5.Text = "label5";
-            // 
-
             // StockExchange
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1088, 674);
-
-            this.Controls.Add(this.label5);
-
-            this.Controls.Add(this.label6);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.checkBox1);
@@ -271,9 +221,5 @@
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label5;
     } }
