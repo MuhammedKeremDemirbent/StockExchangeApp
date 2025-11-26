165fa5b167e6416c079a610c0a7ea7d720cd4691
Muhammed Kerem <keremdmrbnt03@gmail.com>
2025-11-26 22:18:05 +0300

    Conflict resolved

--- StockExchange.cs
+++ StockExchange.cs
@@ -29,9 +29,15 @@ namespace StockExchangeApp
             while (true)
             {
                 //lblAKSEN.Text = await pull_datas.GetQuoteAsync(txtbxVeri_isteme.Text + ".IS");
+<<<<<<< HEAD
                // lblAKSEN.Text = await pull_datas.GetQuoteAsync("AMZN");
 
                // lblGARAN.Text = await pull_datas.GetQuoteAsync("AAPL");
+=======
+                //lblAKSEN.Text = await pull_datas.GetQuoteAsync("AMZN");
+
+                //lblGARAN.Text = await pull_datas.GetQuoteAsync("AAPL");
+>>>>>>> krm
                 await Task.Delay(3000);
             }
         }
